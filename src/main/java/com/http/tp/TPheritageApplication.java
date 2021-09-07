afc6029f2d0de7530b4d0ef456e4ff23d25a4d60
Khalil Lakhdhar <khalillakhdharatc@gmail.com>
2021-09-07 12:42:04 +0100

    Initial commit

---
+++ src/main/java/com/http/tp/TPheritageApplication.java
@@ -0,0 +1,13 @@
+package com.http.tp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TPheritageApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TPheritageApplication.class, args);
+	}
+
+}
