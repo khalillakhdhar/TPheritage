5a42dbad905f9880b2980c1f2b66bd15cd0623c4
Khalil Lakhdhar <khalillakhdharatc@gmail.com>
2021-09-07 13:42:39 +0100

    Update PassengerVehicule.java

--- src/main/java/com/http/tp/model/PassengerVehicule.java
+++ src/main/java/com/http/tp/model/PassengerVehicule.java
@@ -5,4 +5,16 @@ import javax.persistence.MappedSuperclass;
 @MappedSuperclass
 public class PassengerVehicule extends Vehicule {
 private int noofpassengers;
+
+public int getNoofpassengers() {
+	return noofpassengers;
+}
+
+public void setNoofpassengers(int noofpassengers) {
+	this.noofpassengers = noofpassengers;
+}
+
+
+
+
 }
